5620912e82391643d7e8a072851ba8bd936b8977
LandingEagle <github@eswar.at>
2016-11-12 18:51:39 +0100

    Ausgabe Band/Album verallgemeinern, mehr lokale Variablen verwenden

--- mmTools.2016-11-02/Module1.vb
+++ mmTools.2016-11-02/Module1.vb
@@ -1,45 +1,109 @@
-﻿
-REM ********************************************************************************************************************************************************************
-Imports System.Text
-REM ********************************************************************************************************************************************************************
-
-REM ********************************************************************************************************************************************************************
+﻿Imports System.Text
 Imports SongsDB
-REM ********************************************************************************************************************************************************************
 
-REM ********************************************************************************************************************************************************************
 Module Module1
-   REM ****************************************************************************************************************************************************************
-
-   Public mm As SongsDB.SDBApplication = New SongsDB.SDBApplication()
-   Public sl As New SDBSongList
 
    Sub Main()
+
       Dim Result As Boolean
 
-      Do While 1 = 1
-         sl = mm.AllVisibleSongList
-         Result = LstCurTrk(sl)
-      Loop
+      Dim mm As SongsDB.SDBApplication = New SongsDB.SDBApplication()
+      Dim sl As SDBSongList = mm.AllVisibleSongList
+
+      Result = LstAlbMus(sl)
+      Result = LstAlb(sl)
+      Result = LstCurTrk(sl)
 
    End Sub
 
-   Function LstCurTrk(sl As SDBSongList) As Boolean
+   Function LstAlbMus(sl As SDBSongList) As Boolean
+
+      Dim sd As New SDBSongData
+
+      Dim slCnt As Integer = sl.Count - 1
+      Dim Idx As Integer
+
+      Dim L3 As String = Nothing
+
+      Console.Clear()
+      Console.WriteLine("AlbumArtist-Übersicht")
+      Console.WriteLine()
+
+      For Idx = 0 To slCnt
+
+         sd = sl.Item(Idx)
+
+         If L3 <> CStr(sd.AlbumArtistName) Then
+
+            L3 = CStr(sd.AlbumArtistName)
+            dspAlbumArtist(sd)
+
+         End If
+
+      Next
+
+      Return True
+
+   End Function
+
+   Function LstAlb(sl As SDBSongList) As Boolean
 
       Dim sd As New SDBSongData
       Dim sdA As ISDBAlbum
 
       Dim slCnt As Integer = sl.Count - 1
       Dim Idx As Integer
-      Dim AlbumHeader As New StringBuilder
+
+      Dim L2 As String = Nothing
+      Dim L3 As String = Nothing
+
+      Console.Clear()
+      Console.WriteLine("Album-Übersicht")
+      Console.WriteLine()
+
+      For Idx = 0 To slCnt
+
+         sd = sl.Item(Idx)
+         sdA = sd.Album
+
+         If L3 <> CStr(sd.AlbumArtistName) Then
+
+            dspAlbumArtist(sd)
+
+            L3 = CStr(sd.AlbumArtistName)
+            L2 = Nothing
+
+         End If
+
+         If L2 <> CStr(sdA.Name) Then
+
+            dspAlbum(sd)
+
+            L2 = CStr(sdA.Name)
+
+         End If
+      Next
+
+      Return True
+
+   End Function
+
+   Function LstCurTrk(sl As SDBSongList) As Boolean
+
+
+      Dim sd As New SDBSongData
+      Dim sdA As ISDBAlbum
+
+      Dim slCnt As Integer = sl.Count - 1
+      Dim Idx As Integer
+
       Dim TrackData As New StringBuilder
 
       Dim L1 As String = Nothing
       Dim L2 As String = Nothing
       Dim L3 As String = Nothing
 
-      Dim CurLinLng As Integer = 0
-
+      Console.Clear()
       Console.WriteLine("Track-Übersicht")
       Console.WriteLine()
 
@@ -50,13 +114,7 @@ Module Module1
 
          If L3 <> CStr(sd.AlbumArtistName) Then
 
-            Console.WriteLine()
-
-            AlbumHeader.Clear()
-            AlbumHeader.Append("AlbumArtist: ")
-            AlbumHeader.Append(Trim(CStr(sd.AlbumArtistName)))
-            ChgConsBufLng(AlbumHeader.Length)
-            Console.WriteLine(AlbumHeader.ToString)
+            dspAlbumArtist(sd)
 
             L3 = CStr(sd.AlbumArtistName)
             L2 = Nothing
@@ -66,61 +124,93 @@ Module Module1
 
          If L2 <> CStr(sdA.Name) Then
 
-            Console.WriteLine()
-
-            AlbumHeader.Clear()
-            AlbumHeader.Append("Album: ")
-            AlbumHeader.Append(Trim(CStr(sdA.Name)))
-            ChgConsBufLng(AlbumHeader.Length)
-            Console.WriteLine(AlbumHeader.ToString)
-
-            AlbumHeader.Clear()
-            AlbumHeader.Append("Erscheinungs-Datum: ")
-            AlbumHeader.Append(Format(sd.Date, "yyyy-MM-dd"))
-            ChgConsBufLng(AlbumHeader.Length)
-            Console.WriteLine(AlbumHeader.ToString)
+            dspAlbum(sd)
 
             L2 = CStr(sdA.Name)
             L1 = Nothing
 
          End If
 
-         If L1 <> CStr(sd.DiscNumberStr) Then
-
+         If L1 <> CStr(sd.DiscNumberStr) And L1 <> Nothing Then
             Console.WriteLine()
-
-            L1 = CStr(sd.DiscNumberStr)
-
          End If
+         L1 = CStr(sd.DiscNumberStr)
 
          TrackData.Clear()
          If sd.DiscNumber <> 0 Then
             TrackData.Append(Format(sd.DiscNumber, "00"))
-            TrackData.Append(" ")
+         Else
+            TrackData.Append("  ")
          End If
 
+         TrackData.Append(" ")
          TrackData.Append(Format(sd.TrackOrder, "00"))
          TrackData.Append(" ")
          TrackData.Append(sd.SongLengthString)
          TrackData.Append(" ")
          TrackData.Append(sd.Title)
-         ChgConsBufLng(TrackData.Length)
-         Console.WriteLine(TrackData.ToString)
-
+         WrtCnsLin(TrackData)
       Next
 
       Return True
 
    End Function
 
-   Function ChgConsBufLng(LinLng As Integer) As Boolean
-      LinLng += 1
-      If LinLng > Console.BufferWidth Then
-         Console.BufferWidth = LinLng
-      End If
+   Function dspAlbumArtist(sd As SDBSongData) As Boolean
+
+      Dim Header As New StringBuilder
+
+      Console.ForegroundColor = ConsoleColor.Yellow
+      Console.WriteLine()
+
+      Header.Clear()
+      Header.Append(Trim(CStr(sd.AlbumArtistName)))
+      WrtCnsLin(Header)
+
+      Console.ForegroundColor = ConsoleColor.White
 
       Return True
 
+   End Function
+
+   Function dspAlbum(sd As SDBSongData) As Boolean
+
+      Dim Header As New StringBuilder
+      Dim sdA As ISDBAlbum = sd.Album
+
+      Console.ForegroundColor = ConsoleColor.Magenta
+      Console.WriteLine()
+
+      Header.Clear()
+      Header.Append("Album: ")
+      Header.Append(Trim(CStr(sdA.Name)))
+      WrtCnsLin(Header)
+
+      Header.Clear()
+      Header.Append("Erscheinungs-Datum: ")
+
+      If Month(sd.Date) <> 1 And Day(sd.Date) <> 1 Then
+         Header.Append(Format(sd.Date, "yyyy-MM-dd"))
+      ElseIf Month(sd.Date) <> 1 Then
+         Header.Append(Format(sd.Date, "yyyy-MM"))
+         Header.Append("   ")
+      Else
+         Header.Append(Format(sd.Date, "yyyy"))
+         Header.Append("      ")
+      End If
+
+      WrtCnsLin(Header)
+
+      Console.ForegroundColor = ConsoleColor.White
+
+      Return True
+
+   End Function
+
+   Function WrtCnsLin(InpStr As StringBuilder) As Boolean
+      If Console.BufferWidth <= InpStr.Length Then Console.BufferWidth = (InpStr.Length + 1)
+      Console.WriteLine(InpStr.ToString)
+      Return True
    End Function
 
 End Module
